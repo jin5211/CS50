67c9e33150b3e109245faf7f5ad044f23a77011f
zin koko <zinkoko5211@gmail.com>
2025-03-03 23:54:42 +0900

    Week4@20250303_00

---
+++ Week4/lecture/addresses.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n = 50;
+    int *p = &n;
+    char *s = "HI!";
+    printf("n : %i\n", n);
+    printf("&n : %p\n", &n);
+    printf("*p : %i\n", *p);
+    printf("p : %p\n", p);
+    printf("&p : %p\n", &p);
+    printf("s : %s\n", s);
+    printf("s : %p\n", s);
+    printf("*s : %c\n", *s);
+    printf("&s[0] : %p\n", &s[0]);
+    printf("&s[1] : %p\n", &s[1]);
+    printf("&s[2] : %p\n", &s[2]);
+    printf("&s[3] : %p\n", &s[3]);
+    printf("s[0] : %c\n", s[0]);
+    printf("s[1] : %c\n", s[1]);
+    printf("s[2] : %c\n", s[2]);
+    printf("s[3] : %i\n", s[3]);
+    printf("s[0] : %c\n", *s);
+    printf("s[0] : %c\n", *(s + 1));
+    printf("s[0] : %c\n", *(s + 2));
+    printf("s[0] : %i\n", *(s + 3));
+    printf("%s\n", s);
+    printf("%s\n", s + 1);
+    printf("%s\n", s + 2);
+    printf("%s\n", s + 3);
+    return 0;
+}
---
+++ Week4/lecture/copy.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main(void)
+{
+    char *s = get_string("s: ");
+
+    char *t = s;
+
+    t[0] = toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
